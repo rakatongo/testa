91a79944c6e45c37e2e388e825abd7b7abe82825
rakatongo <rakatongo@hotmail.com>
2012-10-27 06:57:12 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testa::Application.config.secret_token = 'f18be89577e289bfc38d460bfbdb92b5e7814dfb68bcc41fd4086506884fd0e3a0052e3f2e80d303ee8aa99eb0ee8e6faafee1265d6c5c6785cf5ab968de0cd5'
